1a63327c0c92a652f67ec8cb1df78146fd1695f3
Steven Lee <Steven@blueberry.lan>
2018-05-27 14:28:01 -0700

    Fix .last function

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,11 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length - 1] : array.slice(array.length - n, array.length);
+    if( array.length - n < 0 ){
+      return array;
+    } else {
+      return n === undefined ? array[array.length - 1] : array.slice(array.length - n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
